8cea403e0848b3cfefe8b42595c26cea20c916cb
pazyu <oleksii.paziuk@sweetcode.pro>
2022-08-02 18:26:32 +0300

    create app-filter component as functionality

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,46 +1,27 @@
-import { render } from "@testing-library/react";
-import { Component } from "react/cjs/react.development";
 import "./app-filter.css";
 
-class AppFilter extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            filter: props.currentFilter
-        }
-    }
+const AppFilter = (props) => {
 
-    onFilterClick = (e) => {
-        const currentFilter = e.target.getAttribute('data-filter');
-        this.setState({
-            filter: currentFilter
-        })
-        this.props.onFilterClick(currentFilter)
-    }
-
-    showButton = (item, count) => {
+    const showButton = (item, count) => {
         const activeClass="btn btn-light";
         const defaultClass = "btn btn-outline-light";
-        const {filter} = this.state;
-        const {currentFiter} = this.props;
+        const {currentFilter, onFilterClick} = props;
         return (
             <button type="button"
                     key={count}
-                    className={item.type === filter ? activeClass : defaultClass}
-                    onClick={this.onFilterClick}
+                    className={item.type === currentFilter ? activeClass : defaultClass}
+                    onClick={() => onFilterClick(item.type)}
                     data-filter={item.type}>
                     {item.title}
                     </button>
         )
     }
 
-    render() {
-        return (
-            <div className="btn-group">
-                {this.props.filters.map((item, count) => this.showButton(item, count))}
-            </div>
-        )
-    }
+    return (
+        <div className="btn-group">
+            {props.filters.map((item, count) => showButton(item, count))}
+        </div>
+    )
 }
 
 export default AppFilter;
